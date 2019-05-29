25d5eaf07655fd96b86fe94a4c27132c21d0a8bc
shenysun <shenysun@163.com>
2019-05-29 22:50:31 +0800

    修改Main

--- src/Main.ts
+++ src/Main.ts
@@ -28,9 +28,6 @@
 //////////////////////////////////////////////////////////////////////////////////////
 
 class Main extends egret.DisplayObjectContainer {
-
-
-
     public constructor() {
         super();
         this.addEventListener(egret.Event.ADDED_TO_STAGE, this.onAddToStage, this);
