ffa5ffdf546eaca27129b82fb663f33690782d5d
shenysun <shenysun@163.com>
2019-05-29 22:53:58 +0800

    nnn

--- src/Main.ts
+++ src/Main.ts
@@ -28,6 +28,9 @@
 //////////////////////////////////////////////////////////////////////////////////////
 
 class Main extends egret.DisplayObjectContainer {
+
+
+
     public constructor() {
         super();
         this.addEventListener(egret.Event.ADDED_TO_STAGE, this.onAddToStage, this);
